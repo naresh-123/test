b6f43301ad8e9c5c815f5bf091e9c693ef131f79
naresh-123 <48730681+naresh-123@users.noreply.github.com>
2022-12-26 18:05:38 +0530

    deteted

--- NewStudentProject/src/app/label-constants.ts
+++
@@ -1,6 +0,0 @@
-export class UserDefinedLabels {
-    static flex_set =  22;
-      static feildAppearance = 'legacy';
-        static readonly floatLabel = 'auto'; 
-        static readonly records_per_page = 10;
-    }
